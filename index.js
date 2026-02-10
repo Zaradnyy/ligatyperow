1e6c8f0946d962a8e2cd2397698439fd290da8fd
Patryk <patryk.rajdukowski@wp.pl>
2026-02-11 00:25:16 +0100

    Update root endpoint message

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
+// middleware
+app.use(express.json());
+
+// testowy endpoint
 app.get("/", (req, res) => {
-  res.send("API działa 🚀");
+  res.send("API działa 🚀 + MongoDB");
 });
+
+// połączenie z MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Połączono z MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ Błąd MongoDB:", err);
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server działa na porcie ${PORT}`);
+  console.log(`🚀 Server działa na porcie ${PORT}`);
 });
